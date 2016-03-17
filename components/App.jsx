ff17390c9ba21eb4cce8cd39bd4b0d4a432fdeb1
Juan Santa Cruz <jhsantacruz@icloud.com>
2016-03-17 01:01:47 -0700

    Created the user event callbacks

--- components/App.jsx
+++ components/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
 import UserSection from './users/UserSection.jsx';
 import MessageSection from './messages/MessageSection.jsx';
+import Socket from '../socket.js';
 
 class App extends Component{
   constructor(props){
@@ -15,38 +16,55 @@ class App extends Component{
     };
   }
   componentDidMount(){
-    let ws = this.ws = new WebSocket('ws://echo.websocket.org');  
+    let socket = this.socket = new Socket();
+    socket.on('connect', this.onConnect.bind(this));
+    socket.on('disconnect', this.onDisconnect.bind(this));
+    socket.on('channel add', this.onAddChannel.bind(this));
+    socket.on('user add', this.onAddUser.bind(this));
+    socket.on('user remove', this.onRemoveUser.bind(this));
   }
-
+  onRemoveUser(removeUser){
+    let {users} = this.state;
+    users = users.filter(user => {
+      return user.id !== removeUser.id;
+    });
+    this.setState({users});
+  }
+  onAddUser(user){
+    let {users} = this.state;
+    users.push(user);
+    this.setState({users});
+  }
+  onEditUser(editUser){
+    let {users} = this.state;
+    users = users.map(user => {
+      if(editUser.id === user.id){
+        return editUser;
+      }
+      return user;
+    });
+    this.setState({users});
+  }
+  onConnect(){
+    this.setState({connected: true});
+  }
+  onDisconnect(){
+    this.setState({connected: false});
+  }
   newChannel(channel){
     let {channels} = this.state;
     channels.push(channel);
     this.setState({channels});
   }
   addChannel(name){
-    let {channels} = this.state;
-    // channels.push({id: channels.length, name});
-    // this.setState({channels});
-    // TODO: Send to server
-    let msg = {
-      name: 'channel add',
-      data: {
-        id: channels.length,
-        name
-      }
-    }
-    this.ws.send(JSON.stringify(msg));
+    this.socket.emit('channel add', {name});
   }
   setChannel(activeChannel){
     this.setState({activeChannel});
     // TODO: Get Channels Messages
   }
   setUserName(name){
-    let {users} = this.state;
-    console.log('Users = ', users)
-    users.push({id: users.length, name});
-    this.setState({users});
-    // TODO: Send to server
+    this.socket.emit('user edit', {name});
   }
   addMessage(body){
     let {messages, users} = this.state;
